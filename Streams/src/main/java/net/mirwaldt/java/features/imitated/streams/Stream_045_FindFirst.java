c7295e67e42aed249c4de623daa24f8af2fbcd94
mmirwaldt <michael.mirwaldt@gmail.com>
2022-03-20 00:31:28 +0100

    added stream examples for the findFirst() terminal operation

---
+++ Streams/src/main/java/net/mirwaldt/java/features/imitated/streams/Stream_045_FindFirst.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2022, Michael Mirwaldt. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">
+ * <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" />
+ * </a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">
+ * Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
+ */
+
+package net.mirwaldt.java.features.imitated.streams;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import static net.mirwaldt.java.features.imitated.util.Utils.middleLine;
+
+public class Stream_045_FindFirst {
+    public static void main(String[] args) {
+        var names = new String[]{"Heinz", "Michael", "Brian", "Marc"};
+
+        // with stream
+        var streamResult = Arrays.stream(names)
+                .findFirst();
+        streamResult.ifPresent(System.out::println);
+
+
+        System.out.println(middleLine());
+
+
+        // without stream
+        Optional<String> nonStreamResult = Optional.empty();
+        if(0 < names.length) {
+            nonStreamResult = Optional.of(names[0]);
+        }
+        nonStreamResult.ifPresent(System.out::println);
+    }
+}
---
+++ Streams/src/main/java/net/mirwaldt/java/features/imitated/streams/Stream_046_FindFirstFiltered.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2022, Michael Mirwaldt. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">
+ * <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" />
+ * </a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">
+ * Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
+ */
+
+package net.mirwaldt.java.features.imitated.streams;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import static net.mirwaldt.java.features.imitated.util.Utils.middleLine;
+
+public class Stream_046_FindFirstFiltered {
+    public static void main(String[] args) {
+        var names = new String[]{"Heinz", "Michael", "Brian", "Marc"};
+
+        // with stream
+        var streamResult = Arrays.stream(names)
+                .filter(s -> s.startsWith("M"))
+                .findFirst();
+        streamResult.ifPresent(System.out::println);
+
+
+        System.out.println(middleLine());
+
+
+        // without stream
+        Optional<String> nonStreamResult = Optional.empty();
+        for (String name : names) {
+            if(name.startsWith("M")) {
+                nonStreamResult = Optional.of(name);
+                break;
+            }
+        }
+        nonStreamResult.ifPresent(System.out::println);
+    }
+}
